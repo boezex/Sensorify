a506a76857d72c1c6a107af98a9d1e0bacc6516c
Daan Boezeman <daan@boezemail.nl>
2022-02-06 11:59:40 +0100

    reworked update thread

--- gui.py
+++ gui.py
@@ -8,24 +8,26 @@ class GUI:
 
     CONSTANTS = 0
 
+    def __init__ (self, pressureSensor):
+        self.pressureSensor = pressureSensor
+
+        self.window = Tk ()
+        self.window.title ("Sensorify (+config version)")
+
+        Label (self.window, text="Differential pressure (actual, Pa): ").grid (row = 0)
+        Label (self.window, text="Temperature (actual, dgrC): ").grid (row = 1)
+
+        self.diffPressActualLabel = Label (self.window, text="0")
+        self.diffPressActualLabel.grid (row=0, column=1)
+        self.tempActualLabel = Label (self.window, text="0")
+        self.tempActualLabel.grid (row=1, column=1)
+
+        self.updateThread = Thread (target=self.updateGui, daemon=True)
+        self.updateThread.start ()
+
+        self.window.mainloop ()
+
     def updateGui (self):
         while True:
             self.diffPressActualLabel["text"] = str(self.pressureSensor.readPressure())
             sleep(1)
-
-    def __init__ (self, pressureSensor):
-        self.pressureSensor = pressureSensor
-        
-        window = Tk ()
-        window.title ("Sensorify (+config version)")
-
-        Label (window, text="Differential pressure (actual, Pa): ").grid (row = 0)
-        Label (window, text="Temperature (actual, dgrC): ").grid (row = 1)
-
-        self.diffPressActualLabel = Label (window, text="0").grid (row=0, column=1)
-        self.tempActualLabel = Label (window, text="0").grid (row=1, column=1)
-
-        updateThread = Thread (target=self.updateGui, daemon=True)
-        updateThread.start ()
-
-        window.mainloop ()
