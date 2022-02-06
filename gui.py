bb416337ad151e4ea05f7672e536b7066254f17d
Daan Boezeman <daan@boezemail.nl>
2022-02-06 11:34:11 +0100

    Reworked updateThread

--- gui.py
+++ gui.py
@@ -1,22 +1,14 @@
 from tkinter import *
+from turtle import update
 from pressuresensor import *
 from time import *
 import threading
 
-class GuiUpdater (threading.Thread):
 
-   def __init__ (self, threadID, name, diffPressActual, tempActual, pressureSensor):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.diffPressActual = diffPressActual
-        self.tempActual = tempActual
-        self.pressureSensor = pressureSensor
-
-   def run (self):
-        while True:
-            self.diffPressActual = PressureSensor.readPressure()
-            sleep(1)
+def updateGui (diffPressActual, tempActual, pressureSensor):
+    while True:
+        diffPressActual = pressureSensor.readPressure()
+        sleep(1)
 
 
 
@@ -30,10 +22,10 @@ class GUI:
         window.title ("Sensorify (+config version)")
         self.diffPressActual = 0
         self.tempActual = 0
-        Label (window, text="Differntial pressure (actual, Pa): ").grid (row = 0)
+        Label (window, text="Differential pressure (actual, Pa): ").grid (row = 0)
         Label (window, text="Temperature (actual, dgrC): ").grid (row = 1)
         Label (window, text=self.diffPressActual).grid (row=0, column=1)
         Label (window, text=self.tempActual).grid (row=1, column=1)
+        thread1 = Thread (target=updateGui, args= (self.diffPressActual, self.tempActual, self.pressureSensor), daemon=True)
+        thread1.start ()
         window.mainloop ()
-        thread1 = GuiUpdater (1, "updateThread", self.diffPressActual, self.tempActual, self.pressureSensor)
-        thread1.start()
